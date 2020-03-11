2d60414675cb5d87ba550a1083fde5bfcf02236e
robertjankowski <robjanw11@wp.pl>
2020-03-11 10:19:15 +0100

    calculate weighted opinion

--- src/real_q_voter/load_graph.py
+++ src/real_q_voter/load_graph.py
@@ -12,23 +12,54 @@ def add_random_opinions(g: nx.Graph):
 
     :param g: nx.Graph
     """
-    for i in range(len(g.nodes())):
-        g.nodes[i]['opinion'] = np.random.choice([-1, 1], 1)
+    for node in g.nodes():
+        g.nodes[node]['opinion'] = np.random.choice([-1, 1], 1)
 
 
-def calculate_magnetization(g: nx.Graph):
+def _has_not_opinion(g: nx.Graph) -> bool:
     """
-    Calculate magnetization (mean opinion value) of the nodes in graph
+    Check if `g` has `opinion` attribute
 
     :param g: nx.Graph
-    :return: magnetization
+    :return: True if `g` has `opinion` attribute, False otherwise
     """
-    has_opinion = nx.get_node_attributes(g, 'opinion')
-    if not has_opinion:
-        logger.error("Cannot calculate magnetization. Graph `g` has not opinion attributes.")
+    if not nx.get_node_attributes(g, 'opinion'):
+        return True
+    return False
+
+
+def calculate_mean_opinion(g: nx.Graph):
+    """
+    Calculate mean opinion <s> of the nodes in graph
+
+    :param g: nx.Graph
+    :return: mean opinion: float
+    """
+    if _has_not_opinion(g):
+        logger.error("Cannot calculate mean opinion. Graph `g` has not attribute: `opinion`")
         return
     opinions = np.array(list(nx.get_node_attributes(g, 'opinion').values()))
     return np.mean(opinions)
+
+
+def calculate_weighted_mean_opinion(g: nx.Graph):
+    """
+    Calculate weighted mean opinion <s * k> of the nodes in graph, where `k` is node's degree
+
+    :param g: nx.Graph
+    :return: weighted mean opinion: float
+    """
+    if _has_not_opinion(g):
+        logger.error("Cannot calculate weighted mean opinion. Graph `g` has not attribute: `opinion`")
+        return
+    weights = []
+    opinions = []
+    for node in g.nodes():
+        opinion = g.nodes[node]['opinion']
+        weight = g.degree[node]
+        weights.append(weight)
+        opinions.append(opinion)
+    return np.average(np.array(opinions), weights=np.array(opinions))
 
 
 def ba_graph(n, m):
@@ -37,23 +68,28 @@ def ba_graph(n, m):
     return g
 
 
+def plot_degree_distribution(g, bins=50):
+    degrees = list(map(lambda x: x[1], g.degree()))
+    degrees = np.histogram(degrees, bins=bins)
+    plt.plot(degrees[1][:-1], degrees[0])
+    plt.xscale('log')
+    plt.yscale('log')
+    plt.show()
+
+
 def main():
     g = nx.read_edgelist("../../data/soc-wiki-Vote.mtx")
+    add_random_opinions(g)
     # nx.draw(g)
     # plt.show()
 
     ba = ba_graph(1e4, 4)
     # nx.draw(ba, node_size=30, alpha=0.2)
 
-    res = calculate_magnetization(ba)
+    res = calculate_weighted_mean_opinion(ba)
     print(res)
 
-    degrees = list(map(lambda x: x[1], g.degree()))
-    degrees = np.histogram(degrees, bins=50)
-    plt.plot(degrees[1][:-1], degrees[0])
-    plt.xscale('log')
-    plt.yscale('log')
-    plt.show()
+    plot_degree_distribution(g)
 
 
 if __name__ == "__main__":
